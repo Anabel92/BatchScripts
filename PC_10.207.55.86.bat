c4fc80adb3ace4238b00a1464880c268ec33efe1
Anabel Mejias <mejias@Anabels-MacBook-Pro-3.local>
2020-11-04 17:20:49 -0500

    Adding batch scripts

---
+++ PC_10.207.55.86.bat
@@ -0,0 +1,3 @@
+netsh interface ipv4 add address "Local Area Connection" 10.207.55.86 255.255.254.0 10.207.54.1
+FOR /L %%A IN (101,1,105) DO netsh interface ipv4 add address "Local Area Connection 2" 10.0.7.%%A 255.255.0.0 10.0.0.1
+FOR /L %%A IN (106,1,110) DO netsh interface ipv4 add address "Local Area Connection 3" 10.0.7.%%A 255.255.0.0 10.0.0.1
